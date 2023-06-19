b902629d17032a968a91beaf729c9b35a4e5c5e3
tomaszochova <TomasJancat@s.zochova.sk>
2023-06-19 11:49:27 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
