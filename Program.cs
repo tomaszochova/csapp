f7510c7bafdf22bf6f44ae2797accd8dc7ee4b83
tomaszochova <TomasJancat@s.zochova.sk>
2023-06-19 12:07:05 +0200

    getip

--- Program.cs
+++ Program.cs
@@ -1,14 +1,34 @@
 ﻿using System;
+using System.Net;
 using System.Threading;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace MyApp
 {
     internal class Program
     {
+        [Obsolete]
+        static void GetIp() {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    string ip = client.DownloadString("https://api.ipify.org");
+                    Console.WriteLine("Your public IPv4 address is: " + ip);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+
+        }
+
+        [Obsolete]
         static void Main(string[] args)
         {
             while (true) {
-                Console.WriteLine("Hello World!");
+                
+                GetIp();
 
                 Thread.Sleep(TimeSpan.FromSeconds(10));
             }
